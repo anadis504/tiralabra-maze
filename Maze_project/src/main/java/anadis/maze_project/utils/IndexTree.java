1fd51a855c814921c8b97f90eaf94d2e4b729128
anadis504 <annadiseth@gmail.com>
2020-10-10 00:52:11 +0300

    Commiting the mess of a Index tree class I got so far

--- Maze_project/src/main/java/anadis/maze_project/utils/IndexTree.java
+++ Maze_project/src/main/java/anadis/maze_project/utils/IndexTree.java
@@ -10,5 +10,71 @@ package anadis.maze_project.utils;
  * @author anadis
  */
 public class IndexTree {
-    
+
+    private int[] tree;
+    private int size, rows, cols, start;
+    private int[] reductions = {2, 1};
+
+    public IndexTree(int rows, int cols) {
+        this.size = rows * cols;
+        this.cols = cols;
+        this.rows = rows;
+        int s = (int)Math.pow(2, Math.ceil(Math.log(2 * size) / Math.log(2)));
+        this.tree = new int[s];
+        this.start = s / 2;
+        for (int i = start + size-1; i > 0; i--) {
+            tree[i] = 2;
+        }
+    }
+
+    public int[] getFreeIndex(int r, int c) {
+        int[] cell = new int[2];
+        cell[0] = r;
+        cell[1] = c;
+        int index = (r - 1) * this.cols + c - 1;
+        index += start-1;
+        if (tree[index] != 0) {
+            return cell;
+        }
+        int s = index / 2;
+        while (s < start) {
+            if (s==1 && tree[s] == 0) {
+                cell[0] = 0;
+                cell[1] = 0;
+                return cell;
+            }
+            if (tree[s] != 0) {
+                if (tree[s * 2 + 1] != 0) {
+                    s = s * 2 + 1;
+                } else if (tree[s*2] != 0) {
+                    s = s * 2;
+                }
+            } else {
+                s /= 2;
+            }
+        }
+        s -= start-1;
+        cell[0] = s / this.cols + 1;
+        cell[1] = s % this.cols;
+        return cell;
+    }
+
+    public void markVisited(int r, int c) {
+        int index = (r - 1) * this.cols + c;
+        index += start-1;
+        tree[index] = 0;
+        int s = index / 2;
+        while (s > 0) {
+            if (tree[s] < 2) {
+                tree[s] = 0;
+                index = s;
+                s /= 2;
+                continue;
+            } else {
+                tree[s]--;
+                break;
+            }
+        }
+    }
+
 }
---
+++ Maze_project/src/test/java/anadis/maze_project/utils/TestIndexTree.java
@@ -0,0 +1,100 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package anadis.maze_project.utils;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author anadis
+ */
+public class TestIndexTree {
+
+    public TestIndexTree() {
+    }
+
+    @BeforeClass
+    public static void setUpClass() {
+    }
+
+    @AfterClass
+    public static void tearDownClass() {
+    }
+
+    @Before
+    public void setUp() {
+    }
+
+    @After
+    public void tearDown() {
+    }
+
+    // TODO add test methods here.
+    // The methods must be annotated with annotation @Test. For example:
+    //
+    // @Test
+    // public void hello() {}
+    @Test
+    public void returnFreeIndexWhenFree() {
+        IndexTree it = new IndexTree(4, 5);
+        int[] cell = it.getFreeIndex(3, 4);
+        System.out.println(cell[0] + ": " + cell[1]);
+
+        assertEquals(cell[0], 3);
+        assertEquals(cell[1], 4);
+    }
+
+    @Test
+    public void returnNewIndexWhenVisited() {
+        IndexTree it = new IndexTree(4, 5);
+        it.markVisited(3, 4);
+        int[] cell = it.getFreeIndex(3, 4);
+        System.out.println(cell[0] + ": " + cell[1]);
+
+        assertEquals(3, cell[0]);
+        assertEquals(3, cell[1]);
+    }
+
+    @Test
+    public void returnFirstIndex() {
+        IndexTree it = new IndexTree(4, 5);
+        it.markVisited(1, 1);
+        int[] cell = it.getFreeIndex(1, 1);
+        System.out.println(cell[0] + ": " + cell[1]);
+
+        assertEquals(1, cell[0]);
+        assertEquals(2, cell[1]);
+    }
+
+    @Test
+    public void returnLastIndex() {
+        IndexTree it = new IndexTree(4, 5);
+        it.markVisited(4, 5);
+        int[] cell = it.getFreeIndex(4, 5);
+        System.out.println(cell[0] + ": " + cell[1]);
+        assertEquals(4, cell[0]);
+        assertEquals(4, cell[1]);
+    }
+
+    public void returnZeroCell() {
+        IndexTree it = new IndexTree(4, 5);
+        for (int i = 1; i <= 4; i++) {
+            for (int j = 1; j <= 5; j++) {
+                it.markVisited(i, j);
+            }
+        }
+        int[] cell = it.getFreeIndex(4, 3);
+        System.out.println(cell[0] + ": " + cell[1]);
+
+        assertEquals(0, cell[0]);
+        assertEquals(0, cell[1]);
+    }
+}
